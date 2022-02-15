611d3a268bec7b7a8c44b786074f6db80ae9e0b0
Nitish0007 <0007nitishsharma@gmail.com>
2022-02-15 14:06:47 +0530

    compliment of base 10 integer

--- complimentOfInteger.cpp
+++ complimentOfInteger.cpp
@@ -31,3 +31,28 @@ int main()
     cout << sol;
     return 0;
 }
+
+// Alternate
+
+// class Solution {
+// public:
+//     int bitwiseComplement(int n) {
+//         if(n == 0){
+//             return 1;
+//         }
+//         int compliment = 0;
+//         int i = 0;
+//         while (n != 0){
+//             int bit;
+//             if(n & 1){
+//                 bit = 0;
+//             }else{
+//                 bit = 1;
+//             }
+//             compliment += (bit * pow(2,i));
+//             n = n >> 1;
+//             i += 1;
+//         }
+//         return compliment;
+//     }
+// };
