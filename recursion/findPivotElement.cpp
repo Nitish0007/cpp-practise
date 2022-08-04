6047117fd1d4fc97f5fb0f4d3c9d4bb0d6691813
Nitish0007 <0007nitishsharma@gmail.com>
2022-08-04 09:33:37 +0530

    question 1, 4th aug 2022

--- recursion/findPivotElement.cpp
+++ recursion/findPivotElement.cpp
@@ -2,25 +2,26 @@
 
 using namespace std;
 
-int findPeakIndex(int arr[], int s, int e, int ans){
+int findPivot(int arr[], int s, int e, int ans){
     if(s >= e){
         return ans;
     }
 
     int mid = s + (e - s)/2;
 
-    if(arr[mid] <= arr[mid + 1]){
-        ans = mid+1;
-        return findPeakIndex(arr, mid + 1, e, ans);
+    if(arr[mid] >= arr[0]){
+        return findPivot(arr, (mid + 1), e, ans);
     }
     else{
-        return findPeakIndex(arr, s, mid, ans);
+        ans = mid;
+        return findPivot(arr, s, mid, ans);
     }
 }
 
 int main(){
-    int n; 
+int n; 
     cout <<"enter size of array : "<<endl;
+    //array must be sorted and rotated
     cin>> n;
     int *arr = new int[n];
      for (int i = 0; i < n; ++i)
@@ -30,7 +31,8 @@ int main(){
         arr[i] = elem;
     }
 
-    cout<<findPeakIndex(arr, 0, (n-1),-1)<<endl;
+    int pivot = findPivot(arr, 0, (n-1),-1);
+    cout<<pivot<<endl;
 
     delete []arr;
 
